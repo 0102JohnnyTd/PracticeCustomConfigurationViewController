287f58ed9a710589e85a762e21ffa03ef992284d
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-16 19:15:31 +0900

    CollectionViewを設置したViewControllerを作成

---
+++ PracticeCustomConfigurationViewController/CustomConfigurationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CustomConfigurationViewController.swift
+//  PracticeCustomConfigurationViewController
+//
+//  Created by Johnny Toda on 2023/02/16.
+//
+
+import UIKit
+
+final class CustomConfigurationViewController: UIViewController {
+    @IBOutlet private weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
