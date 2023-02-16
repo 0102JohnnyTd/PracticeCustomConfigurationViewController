a7035273fb01bbd04cf174e233f44b1618d544de
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-16 17:59:43 +0900

    Initial　commit

---
+++ PracticeCustomConfigurationViewController/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PracticeCustomConfigurationViewController
+//
+//  Created by Johnny Toda on 2023/02/16.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
